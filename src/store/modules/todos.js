90a620e59ce0e8e068fc867599587876ec92909b
drkgrntt <drkgrntt@gmail.com>
2020-08-24 21:14:35 -0500

    Create vuex app

---
+++ src/store/modules/todos.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+
+const state = {
+  todos: []
+}
+
+const getters = {
+  allTodos: state => state.todos
+}
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+
+    commit('setTodos', response.data)
+  },
+  async addTodo({ commit }, title) {
+    const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false })
+
+    commit('newTodo', response.data)
+  },
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+
+    commit('removeTodo', id)
+  },
+  async filterTodos({ commit }, event) {
+    console.log(event.target.value)
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${event.target.value}`)
+
+    commit('setTodos', response.data)
+  },
+  async updateTodo({ commit }, updatedTodo) {
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updatedTodo.id}`, updatedTodo)
+
+    commit('editTodo', response.data)
+  }
+}
+
+const mutations = {
+  setTodos: (state, todos) => state.todos = todos,
+  newTodo: (state, todo) => state.todos.unshift(todo),
+  removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+  editTodo: (state, updatedTodo) => state.todos = state.todos.map(todo => todo.id !== updatedTodo.id ? todo : updatedTodo)
+}
+
+export default {
+  state, getters, actions, mutations
+}
